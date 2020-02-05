07078a99d62d0f0cbe112f1e794bc18b92ee94f6
Matias Wallenius <MatiWall@live.dk>
2020-02-05 22:03:46 +0100

    costumized navigation bar

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ from app import server # Necessary for heroku deployment
 
 logo_style = {'text-align': 'center', 'color': 'white' , 'backgroundColor':'dodgerblue', 'fontSize' : 40, 'font-weight': 'bold', 'font-style': 'italic', 'marginLeft': 20,}
 
-navigation_buttom_style ={'text-align': 'center', 'color': 'white', 'fontSize' : 25, 'marginBottom': 0, 'marginTop': 0, 'border-radius': 2, 'backgroundColor':'dodgerblue', 'marginLeft': 20, 'marginLeft': 10}
+navigation_buttom_style ={'text-align': 'center', 'color': 'white', 'fontSize' : 20, 'marginBottom': 0, 'marginTop': 0, 'marginLeft': 5, 'marginRight': 5}
 navigation_style = {'backgroundColor':'dodgerblue', 'border-radius': 5 , 'marginLeft': 0,}
 
 
@@ -18,9 +18,9 @@ navigation_style = {'backgroundColor':'dodgerblue', 'border-radius': 5 , 'margin
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(html.Nav([	html.A('K&M Investments',  style = logo_style, className = 'tab'), html.A(dcc.Link('Home', href='/', style = navigation_buttom_style), className = 'nav-item nav-link btn', style = {'marginLeft': 30,}), 
-    		  	html.A( dcc.Link('Stock', href='/apps/app1', style = navigation_buttom_style), className = 'nav-item nav-link btn'), 
-    		  	html.A( dcc.Link('Intraday', href='/apps/app2', style = navigation_buttom_style), className = 'nav-item nav-link btn'),], style = navigation_style)),
+    html.Div(html.Nav([	html.A('K&M Investments',  style = logo_style), html.A(dcc.Link('Home', href='/', style = navigation_buttom_style), className = 'button', style = {'marginLeft': 30,}), 
+    		  	html.A( dcc.Link('Stock', href='/apps/app1', style = navigation_buttom_style), className = 'button'), 
+    		  	html.A( dcc.Link('Intraday', href='/apps/app2', style = navigation_buttom_style), className = 'button')], style = navigation_style)),
     		  	
     html.Div(id='page-content')
 ])
--- stock_plots/plots.py
+++ stock_plots/plots.py
@@ -4,17 +4,26 @@ import datetime
 
 
 
-def plot_timeseries(ticker = 'TSLA', roling_mean = True, window = 50, price = 'Close', start_date = None, end_date = None):
-    layout = dict(title = 'Stoch chart', showlegend = False)
+class timeseries_data:
+
+	def __init__(self, ticker = 'TSLA', rolling_mean = True, window = 50, price = 'Close'):
+    
         
+    self.df = pdr.data.get_data_yahoo(ticker, start = start_date, end = end_date)
+    self.ticker = ticker
     
-
-    df = pdr.data.get_data_yahoo(ticker, start = start_date, end = end_date)
+    self.rolling_mean = True
+    self.window = window 
+    self.price = price
     
-    fig = go.Figure(go.Scatter(x = list(df.index), y = list(df[price])), layout)
+    
+    
+    def plot(start_date = None, end_date = None):
+    
+    	fig = go.Figure(go.Scatter(x = list(df.index), y = list(df[price])), layout)
 
-    if roling_mean:
-        df_rm = df.rolling(window, center = True).mean()
-        fig.add_trace(go.Scatter(x = df_rm.index, y = df_rm[price] ))
+    	if roling_mean:
+        	df_rm = df.rolling(window, center = True).mean()
+        	fig.add_trace(go.Scatter(x = df_rm.index, y = df_rm[price] ))
         
     return fig
