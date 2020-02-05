9707bb1207218e72ef43793ee9cfa00e0a2316f0
Matias Wallenius <MatiWall@live.dk>
2020-02-05 19:52:19 +0100

    added navigation bar

--- index.py
+++ index.py
@@ -2,25 +2,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-
 from app import app
 from apps import app1, app2
 
 from app import server # Necessary for heroku deployment
 
 
-header_style = {'text-align': 'center', 'color': 'white' , 'backgroundColor':'dodgerblue', 'fontSize' : 40, 'font-weight': 'bold', 'font-style': 'italic', 'marginBottom': 0, 'marginTop': 0, 'border-radius': 5,}
+logo_style = {'text-align': 'center', 'color': 'white' , 'backgroundColor':'dodgerblue', 'fontSize' : 40, 'font-weight': 'bold', 'font-style': 'italic', 'marginLeft': 20,}
 
-navigation_buttom_style ={'text-align': 'center', 'color': 'white', 'fontSize' : 25, 'marginBottom': 0, 'marginTop': 0, 'border-radius': 2, 'backgroundColor':'dodgerblue'}
-navigation_style = {'backgroundColor':'dodgerblue'}
+navigation_buttom_style ={'text-align': 'center', 'color': 'white', 'fontSize' : 25, 'marginBottom': 0, 'marginTop': 0, 'border-radius': 2, 'backgroundColor':'dodgerblue', 'marginLeft': 20, 'marginLeft': 10}
+navigation_style = {'backgroundColor':'dodgerblue', 'border-radius': 5 , 'marginLeft': 0,}
+
+
 
 
 app.layout = html.Div([
-	html.H1('K&M Investments',  style= header_style),
     dcc.Location(id='url', refresh=False),
-    html.Div(html.Nav([	html.Div(dcc.Link('Home', href='/', style = navigation_buttom_style), className = 'nav-item nav-link btn'), 
-    		  	html.Div( dcc.Link('Stock', href='/apps/app1', style = navigation_buttom_style), className = 'nav-item nav-link btn'), 
-    		  	html.Div( dcc.Link('Intraday', href='/apps/app2', style = navigation_buttom_style), className = 'nav-item nav-link btn')], style = navigation_style)),
+    html.Div(html.Nav([	html.A('K&M Investments',  style = logo_style, className = 'tab'), html.A(dcc.Link('Home', href='/', style = navigation_buttom_style), className = 'nav-item nav-link btn', style = {'marginLeft': 30,}), 
+    		  	html.A( dcc.Link('Stock', href='/apps/app1', style = navigation_buttom_style), className = 'nav-item nav-link btn'), 
+    		  	html.A( dcc.Link('Intraday', href='/apps/app2', style = navigation_buttom_style), className = 'nav-item nav-link btn'),], style = navigation_style)),
     		  	
     html.Div(id='page-content')
 ])
