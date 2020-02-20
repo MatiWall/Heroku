79c49ebd64b1ec2786573e2d99ee52c62b2b2e18
Matias Wallenius <MatiWall@live.dk>
2020-02-20 19:32:03 +0100

    Addes RSI

--- apps/plots.py
+++ apps/plots.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 import pandas as pd
-import plotly.tools as tls
-
+#import plotly.tools as tls
+from plotly.subplots import make_subplots
 import dash_core_components as dcc
 
 
@@ -24,17 +24,44 @@ class timeseries_plot:
         self.end = None 
     
     def plot(self, price = 'Close'):
-       
+        self.price = price
      
-        fig = tls.make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.009,horizontal_spacing=0.009)
+        fig = make_subplots(rows=5, cols=1, shared_xaxes=True, vertical_spacing=0.009,horizontal_spacing=0.009,
+                            specs=[[{"secondary_y": False}],
+                                   [{"secondary_y": True, "rowspan": 3}],  
+                                   [None],
+                                   [None],
+                                   [{"secondary_y": False}] ],
+                                   )
+                               
+        
         
         fig['layout']['margin'] = {'l': 30, 'r': 10, 'b': 50, 't': 25}
-        fig.update_yaxes(title_text = 'Price', row=1, col=1)
+       # fig.update_yaxes(title_text = 'Price', row=1, col=1)
         
         
-        fig.append_trace({'x':self.df.index,'y':self.df.Volume,'type':'bar','name':'Volume', 'showlegend' : False, 'marker' :{'color' : 'red'}},2,1)
-        fig.update_xaxes(title_text="Date", row=2, col=1)
-        fig.update_yaxes(title_text='Counts', row=2, col=1)
+        fig.add_trace({'x':self.df.index,'y':self.df.Volume,'type':'bar','name':'Volume', 'showlegend' : False, 'marker' :{'color' : 'red'}},
+                         row = 2, col = 1, secondary_y=False)
+        fig.update_xaxes(title_text="Date", row = 2, col = 1)
+        fig.update_yaxes(title_text='Counts', row = 2, col = 1)
+        
+        
+        
+        rsi = self.rsiFunc()
+        fig.add_trace({'x' : rsi.index,'y' : rsi.values, 'type' : 'scatter', 'name':'rsi', 'showlegend' : False, 'marker' :{'color' : 'red'}},
+                         row = 1, col = 1)
+        
+        
+        fig.update_yaxes(title_text="RSI",  range=[0, 100], row = 1, col = 1)
+                        
+                        
+                 
+                    
+        emaslow, emafast, macd = self.computeMACD()
+        fig.add_trace({'x' : macd.index,'y' : macd.values, 'type' : 'scatter', 'name':'rsi', 'showlegend' : False, 'marker' :{'color' : 'red'}},
+                         row = 5, col = 1)
+        
+        
         
         return fig
     
@@ -43,24 +70,59 @@ class timeseries_plot:
     def add_plot(self, fig, prices):
         
         for price in is_list(prices):
-            fig.append_trace({'x':self.df.index,'y':self.df[price],'type':'scatter','name': price},1,1)
+            fig.add_trace({'x':self.df.index,'y':self.df[price],'type':'scatter','name': price}, row = 2, col = 1, secondary_y = True)
         
         return fig
         
     
     
-    def bollinger_bands(self, fig, price = 'Close', window = 40, no_std = 2, center = False):
+    def bollinger_bands(self, fig, window = 40, no_std = 2, center = False):
         df_rm = self.df.rolling(window, center = center).mean()
         rolling_std = self.df.rolling(window, center = center ).std()
         
+        price = self.price
         
-        fig = fig.add_trace({'x' : df_rm.index, 'y' : df_rm[price], 'name' : 'mavg'  }, 1,1)
-        fig = fig.add_trace({'x' : df_rm.index, 'y' : df_rm[price]+rolling_std[price]*no_std, 'name' : 'Bb Upper'  }, 1,1)
-        fig = fig.add_trace({'x' : df_rm.index, 'y' : df_rm[price]-rolling_std[price]*no_std, 'name' : 'Bb Lower'  }, 1,1)
+        fig = fig.add_trace({'x' : df_rm.index, 'y' : df_rm[price], 'name' : 'mavg'  }, row = 2,col = 1, secondary_y = True)
+        fig = fig.add_trace({'x' : df_rm.index, 'y' : df_rm[price]+rolling_std[price]*no_std, 'name' : 'Bb Upper'  }, row = 2, col =1, secondary_y = True)
+        fig = fig.add_trace({'x' : df_rm.index, 'y' : df_rm[price]-rolling_std[price]*no_std, 'name' : 'Bb Lower'  }, row = 2, col = 1, secondary_y = True)
         
         return fig
         
     
+    
+    
+    def rsiFunc(self, window = 14):
+        price = self.price
+        
+        delta = self.df[price].diff()
+        up_days = delta.copy()
+        up_days[delta<=0]=0.0
+        down_days = abs(delta.copy())
+        down_days[delta>0]=0.0
+        RS_up = up_days.rolling(window).mean()
+        RS_down = down_days.rolling(window).mean()
+        rsi = 100-100/(1+RS_up/RS_down)
+
+        return rsi
+    	
+    	
+    	
+    	
+    def ExpMovingAverage(self, window = 20):
+        
+        return self.df[self.price].ewm(span = window, adjust=False).mean()
+    
+    
+    
+    def computeMACD(self, slow=26, fast=12):
+        """
+        compute the MACD (Moving Average Convergence/Divergence) using a fast and slow exponential moving avg'
+        return value is emaslow, emafast, macd which are len(x) arrays
+        """
+        emaslow = self.ExpMovingAverage(window = slow)
+        emafast = self.ExpMovingAverage(window = fast)
+        
+        return emaslow, emafast, emafast - emaslow
     
     
     
--- apps/simulations.py
+++ apps/simulations.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-
-
+import plotly.figure_factory as ff
+from datetime import datetime, timedelta 
 
 class monte_carlo:
     
@@ -11,3 +11,50 @@ class monte_carlo:
         self.df = df
         self.start = start
         self.end = end
+        
+        
+    def GBM(self, simulations = 100, forcast_steps = 50, dt=1):
+        ''' 
+        GBM: Geometric Brownian Motion.
+        '''
+        
+        last_price = self.df.iloc[-1]
+        last_date = self.df.index[-1]
+        
+        
+
+        returns = self.df.pct_change()
+        daily_std = returns.std()
+        daily_mean = returns.mean()
+        
+        forcast_steps = int(np.ceil(self.forecast_days/dt))
+
+        df_simulations = pd.DataFrame( index = [ last_date + pd.Timedelta(days=dt*x) for x in range(forcast_steps)])
+    
+        for x in range(simulations):
+        
+            price_series = []
+            price = last_price * np.exp( (daily_mean-daily_std**2.0/2.0)* dt + daily_std *np.sqrt(dt) *np.random.normal(0, 1))
+            price_series.append(price)
+        
+            for i in range(forcast_steps-1):
+            
+                price = price_series[i]*np.exp( (daily_mean-daily_std**2.0/2.0)* dt + daily_std *np.sqrt(dt) *np.random.normal(0, 1))
+                price_series.append(price)
+             
+        
+            df_simulations[x] = price_series
+        
+            
+        self.df_simulations = df_simulations
+        
+       
+       
+	def plot(self):
+	
+		fig = ff.create_distplot(self.df_simulations)
+		
+		
+		return fig
+		
+	
